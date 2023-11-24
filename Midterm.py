5cb8201badbe720b56383e62eb458fb649ab7ced
LunaAlJurdy <149412721+LunaAlJurdy@users.noreply.github.com>
2023-11-24 12:06:37 +0200

    Add files via upload

---
+++ Midterm.py
@@ -0,0 +1,83 @@
+list=["Replit","Github","Stack Overflow","W3Schools"]
+tab={}
+tab["Replit"]="https://replit.com/"
+tab["Github"]="https://github.com/"
+tab["Stack Overflow"]="https://stackoverflow.com/"
+tab["W3Schools"]="https://www.w3schools.com/"
+print(tab)
+print(list)
+def openTab():
+  print("please choose a Title")
+  title=str(input())
+  for i in range(len(list)):
+
+    if title in list:
+     print("the title you chose is present: ",title)
+
+    else:
+      print("the title you chose is added: ",title)
+  print("please choose a link")
+  link=str(input())
+  
+  for i in range(len(tab)):
+    if link in tab:
+      print("the link you chose is present: ",link)
+
+    else:
+      print("the link you chose is added: ",link)
+  
+      list.append(title)
+    
+      return title
+
+def closeTab():
+  print("Enter the index of the tab you would like to close")
+  index=int(input())
+  list.pop(index)
+  print(list)
+
+def switchTab():
+  print("Enter the index of the tab you would like to switch to")
+  index=int(input())
+  print("Switching to tab: ",list[index])
+  return index
+def mainMenu():
+  choice=-99 # dummy value
+  while choice !=9:
+    print("Welcome")
+    print("Enter")
+    print("1. to open tab")
+    print("2. to close tab")
+    print("3. to switch tab")
+    print("4. to display all tabs")
+    print("5. to open nested tab")
+    print("6. to sort all tabs")
+    print("7. to save tabs")
+    print("8. to import tabs")
+    print("9. to exit")
+    
+    choice=int(input())
+
+    if choice==1:
+      openTab()
+    elif choice==2:
+      closeTab()
+    elif choice==3:
+      switchTab()
+    elif choice==4:
+      displayTab()
+    elif choice==5:
+      nestedTab()
+    elif choice==6:
+      sortTab()
+    elif choice==7:
+      saveTab()
+    elif choice==8:
+      importTab()
+    elif choice ==9:
+      print("bye bye")
+    else:
+      print("ivalid input")
+
+mainMenu()
+
